6848dd7ffed7d5add5d2dbe204f8b0c791b686b0
MoZ0311 <mozmegane0311@gmail.com>
2025-01-14 23:56:32 +0900

    Stageの追加

---
+++ Stage.cpp
@@ -0,0 +1,59 @@
+﻿// Stage class
+
+#include "Stage.hpp"
+
+// インスタンスをnullptrで初期化
+Stage* Stage::stageInstance = nullptr;
+
+Stage::Stage()
+{
+
+}
+
+Stage::~Stage()
+{
+
+}
+
+void Stage::Init()
+{
+	if (stageInstance != nullptr)
+	{
+		return;
+	}
+
+	stageInstance = new Stage();
+}
+
+void Stage::Release()
+{
+	if (stageInstance != nullptr)
+	{
+		delete stageInstance;
+		stageInstance = nullptr;
+	}
+}
+
+void Stage::Update()
+{
+	if (KeySpace.down())
+	{
+		Print << U"押された!!";
+	}
+
+	if (KeySpace.up())
+	{
+		ClearPrint();
+	}
+}
+
+
+void Stage::Draw()
+{
+
+}
+
+Stage* Stage::GetStageInstance()
+{
+	return stageInstance;
+}
---
+++ Stage.hpp
@@ -0,0 +1,34 @@
+﻿// Stage class
+
+#pragma once
+
+class Stage
+{
+public:
+
+	// 初期化処理 void(void)
+	static void Init();
+
+	// 解放処理 void(void)
+	static void Release();
+
+	// 更新処理 void(void)
+	void Update();
+
+	// 描画処理 void(void)
+	void Draw();
+
+	// インスタンスのゲッター関数
+	static Stage* GetStageInstance();
+
+private:
+
+	// コンストラクタ
+	Stage();
+
+	// デストラクタ
+	~Stage();
+
+	// シングルトンクラスのインスタンスのポインタ
+	static Stage* stageInstance;
+};
