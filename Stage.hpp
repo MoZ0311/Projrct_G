a16a3ce506213368916bca88976a1de4611bde11
MoZ0311 <mozmegane0311@gmail.com>
2025-01-22 14:58:11 +0900

    CSVファイルの書き換え実装

--- Stage.hpp
+++ Stage.hpp
@@ -102,8 +102,11 @@ private:
 		TILE_OFFSET.y * 2 * TILE_NUM,
 		Vec2{ 0, TILE_OFFSET.y * TILE_NUM - TILE_THICKNESS - TILE_OFFSET.y * 2});
 
-	// CSV ファイルからデータを読み込む
-	const CSV csv{ U"data/sample.csv" };
+	// マップデータのCSVファイル
+	const String MAP_DATA_FILE = U"data/sample.csv";
+
+	// 読み込み対象のCSVファイル
+	CSV csv;
 
 	// タイルの種類
 	Grid<int32> grid;
